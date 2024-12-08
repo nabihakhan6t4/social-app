5c0fe00b73a78093b831506a31502bf3bf2d4c46
nabihakhan6t4 <khannabiha923@gmail.com>
2024-12-08 17:36:20 +0300

    added users profile section

--- home.js
+++ home.js
@@ -1,32 +1,29 @@
-import {
-  getAuth,
-  onAuthStateChanged,
-  signOut
-} from "./firebase.js";
+import { getAuth, onAuthStateChanged, signOut } from "./firebase.js";
 
 const auth = getAuth();
 
 // Check User Authentication
 onAuthStateChanged(auth, (user) => {
   if (user) {
-      if (!user.emailVerified) {
-          window.location.href = "verify-email.html"; // Redirect to verification page if email not verified
-      } else {
-          console.log("User is signed in and email is verified.");
-      }
+    if (!user.emailVerified) {
+      window.location.href = "verify-email.html"; // Redirect to verification page if email not verified
+    } else {
+      console.log("User is signed in and email is verified.");
+    }
   } else {
-      window.location.href = "./index.html"; // Redirect to login page
+    window.location.href = "./index.html"; // Redirect to login page
   }
 });
 
 // Sign Out Functionality
 document.querySelectorAll("#signOut").forEach((btn) =>
   btn.addEventListener("click", async () => {
-      try {
-          await signOut(auth);
-          window.location.href = "./index.html"; // Redirect to login page after sign out
-      } catch (error) {
-          Swal.fire("Error", error.message, "error");
-      }
+    try {
+      await signOut(auth);
+      window.location.href = "./index.html"; // Redirect to login page after sign out
+    } catch (error) {
+      Swal.fire("Error", error.message, "error");
+    }
   })
 );
+
